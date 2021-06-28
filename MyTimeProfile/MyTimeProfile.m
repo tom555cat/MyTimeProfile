31c4be6c650ad1c924f6d64c9711be503e8fbfab
tongleiming <tongleiming1989@sina.com>
2021-06-28 23:05:47 +0800

    添加代码

--- MyTimeProfile/MyTimeProfile.m
+++ MyTimeProfile/MyTimeProfile.m
@@ -7,14 +7,24 @@
 
 #import "MyTimeProfile.h"
 #include "fishhook.h"
+#include <pthread/pthread.h>
 
 // 局部变量，使用的函数指针
 void (*orgin_objc_msgSend)(void);
 
+static pthread_key_t threadKeyLR;
 
 #warning 无限参数的函数怎么表示?
 #warning objc_msgSend不是无限参数吗
 extern void hook_msgSend();
+
+typedef struct {
+    int allocLength;
+    int index;
+    uintptr_t *lr_stack;
+} LRStack;
+
+static struct LRStack *lrStack;
 
 @implementation MyTimeProfile
 
@@ -34,6 +44,38 @@ void hookObjcMsgSend()
     
     struct rebinding rebs[1] = {rebindingObjcMsgSend};
     rebind_symbols(rebs, 1);
+}
+
+void hook_objc_msgSend_before(id self, SEL selector, uintptr_t lr)
+{
+    // 一些记录操作
+    
+    // 记录LR寄存器
+    setLRRegisterValue(lr);
+}
+
+// 为什么用内联函数
+static inline void setLRRegisterValue(uintptr_t lr)
+{
+    // 每个线程独占一个lrStack
+    LRStack *lrStack = pthread_getspecific(threadKeyLR);
+    if (!lrStack) {
+        lrStack = (LRStack *)malloc(sizeof(LRStack));
+        lrStack->allocLength = 128;
+        lrStack->lr_stack = (uintptr_t *)malloc(lrStack->allocLength * sizeof(uintptr_t));
+        lrStack->index = -1;
+        pthread_setspecific(threadKeyLR, lrStack);
+    }
+    if (++lrStack->index >= lrStack->allocLength) {
+        lrStack->allocLength += 128;
+        lrStack->lr_stack = (uintptr_t *)realloc(lrStack->lr_stack, lrStack->allocLength *sizeof(uintptr_t));
+    }
+    lrStack->lr_stack[lrStack->index] = lr;
+}
+
+void hook_objc_msgSend_after(BOOL is_objc_msgSendSuper)   // 为什么这里要判断super
+{
+    
 }
 
 @end
--- MyTimeProfile/hookObjcMsgSend-arm64.s
+++ MyTimeProfile/hookObjcMsgSend-arm64.s
@@ -20,12 +20,62 @@ $0:
 LExit$0:
 .endmacro
 
-.macro COPY_STACK_FRAME
+// 为什么q7~q1取的是#-0x20？
+// 为什么x0~x8是#-0x10?
+.macro BACKUP_REGISTERS
+    stp q6, q7, [sp, #-0x20]!   // 等效于 sub sp, sp, #32; stp q6, q7, [sp]; add ap, sp
+    stp q4, q5, [sp, #-0x20]!
+    stp q2, q3, [sp, #-0x20]!
+    stp q0, q1, [sp, #-0x20]!
+    stp x6, x7, [sp, #-0x10]!
+    stp x4, x5, [sp, #-0x10]!
+    stp x2, x3, [sp, #-0x10]!
+    stp x0, x1, [sp, #-0x10]!
+    str x8,  [sp, #-0x10]!
+.endmacro BACKUP_REGISTERS
     
+.macro RESTORE_REGISTERS
+    ldr x8, [sp], #0x10
+    ldp x0, x1, [sp], #0x10
+    ldp x2, x3, [sp], #0x10
+    ldp x4, x5, [sp], #0x10
+    ldp x6, x7, [sp], #0x10
+    ldp q0, q1, [sp], #0x20
+    ldp q2, q3, [sp], #0x20
+    ldp q4, q5, [sp], #0x20
+    ldp q6, q7, [sp], #0x20
+.endmacro RESTORE_REGISTERS
 
-.endmacro COPY_STACK_FRAME
+.macro CALL_HOOK_AFTER
+    BACKUP_REGISTERS
+    mov x0, #0x0
+    bl hook_objc_msgSend_after
+    mov lr, x0
+    RESTORE_REGISTERS
+.endmacro CALL_HOOK_AFTER
+
+.macro CALL_HOOK_BEFORE
+    BACKUP_REGISTERS
+    mov x2, lr
+    bl _hook_objc_msgSend_before
+    RESTORE_REGISTERS
+.endmacro CALL_HOOK_BEFORE
+
+// 这个跳转比较复杂，没有看懂
+.macro CALL_ORIGIN_OBJC_MSGSEND
+    adrp    x17, _orgin_objc_msgSend@PAGE
+    ldr    x17, [x17, _orgin_objc_msgSend@PAGEOFF]
+    blr x17
+.endmacro CALL_ORIGIN_OBJC_MSGSEND
+
+// 这个是用来显示堆栈的，可以决定用不用
+//.macro COPY_STACK_FRAME
+//.endmacro COPY_STACK_FRAME
 
 ENTRY _hook_msgSend
+    CALL_HOOK_BEFORE
+    CALL_ORIGIN_OBJC_MSGSEND
+    CALL_HOOK_AFTER
 END_ENTRY _hook_msgSend
 
 #endif
